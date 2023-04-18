8beb55146ef1c3b8f17e11c91e4c40b84c5ace0b
toly <1981462002@qq.com>
2023-04-19 07:36:46 +0800

    动画

--- lib/guess/result_notice.dart
+++ lib/guess/result_notice.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ResultNotice extends StatelessWidget {
+class ResultNotice extends StatefulWidget {
   final Color color;
   final String info;
 
@@ -11,16 +11,42 @@ class ResultNotice extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<ResultNotice> createState() => _ResultNoticeState();
+}
+
+class _ResultNoticeState extends State<ResultNotice> with SingleTickerProviderStateMixin{
+
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this,duration: const Duration(milliseconds: 200));
+    controller.forward();
+  }
+
+  @override
+  void didUpdateWidget(covariant ResultNotice oldWidget) {
+    controller.forward(from: 0);
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Expanded(
         child: Container(
-      alignment: Alignment.center,
-      color: color,
-      child: Text(
-        info,
-        style: TextStyle(
-            fontSize: 54, color: Colors.white, fontWeight: FontWeight.bold),
-      ),
-    ));
+          alignment: Alignment.center,
+          color: widget.color,
+          child: AnimatedBuilder(
+            animation: controller,
+            builder: (_,child) => Text(
+              widget.info,
+              style: TextStyle(
+                  fontSize: 54*(controller.value),
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ));
   }
 }
